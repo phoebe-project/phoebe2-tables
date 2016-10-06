7468c189682ab6222c7a1317115c2c678094a3fe
Kyle Conroy <kyleconroy@gmail.com>
2016-10-06 15:53:57 -0400

    stored list is now a dictionary (bolometric doesn't follow by the same naming rules)

--- passbands/devel/update_list.py
+++ passbands/devel/update_list.py
@@ -1,8 +1,13 @@
 import json
+import os
+from glob import glob
 from phoebe.atmospheres import passbands
 
-passbands.init_passbands()
+
+for fname in glob('./*pb'):
+   print fname
+   passbands.init_passband(fname)
 
 f = open('list_online_passbands', 'w')
-f.write(json.dumps(passbands._pbtable.keys()))
+f.write(json.dumps({k: os.path.basename(v['fname']) for k,v in passbands._pbtable.items()}))
 f.close()
