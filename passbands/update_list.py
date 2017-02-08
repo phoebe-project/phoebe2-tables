5e0328b4acc30b2f4b5fa8ae306b11098c9ff632
Kyle Conroy <kyleconroy@gmail.com>
2017-02-08 11:27:24 -0500

    update_list now exposes supported atms
    and also updated both versions (old and new) of the list.  Once released, we can remove the list_online_passbands file as well as its creation within update_list.py

--- passbands/update_list.py
+++ passbands/update_list.py
@@ -8,6 +8,11 @@ for fname in glob('./*pb'):
    print fname
    passbands.init_passband(fname)
 
+# keep this for legacy sake, for now (development version before Feb 8 will require this)
 f = open('list_online_passbands', 'w')
 f.write(json.dumps({k: os.path.basename(v['fname']) for k,v in passbands._pbtable.items()}))
+f.close()
+
+f = open('list_online_passbands_full', 'w')
+f.write(json.dumps({k: {'fname': os.path.basename(v['fname']), 'atms': v['atms']} for k,v in passbands._pbtable.items()}))
 f.close()
