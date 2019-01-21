56d1d4b5101ae09892bc71aeadad6557d1406412
Andrej Prsa <aprsa09@gmail.com>
2019-01-21 15:25:54 -0500

    Passbands converted from python 2's marshal to python 3's pickles. Conversion scripts are also included; convert_step1.py should be run in python2, and convert_step2.py should be run in python3.

---
+++ passbands/convert_step2.py
@@ -0,0 +1,20 @@
+"""
+Run this in python 3 to read in the python 2 pickle passband file and save it as a python 3 pickle.
+"""
+
+import pickle
+import glob
+import time
+import os
+
+for legacy_pb in glob.glob('*.pb'):
+    # Read in the pickled passband:
+    with open(legacy_pb+'.pkl2', 'rb') as f:
+        pb = pickle.load(f, encoding='latin1')
+
+    # Save as a protocol-3 pickle:
+    with open(legacy_pb+'3', 'wb') as f:
+        pickle.dump(pb, f)
+
+    # Delete the conv file:
+    os.remove(legacy_pb+'.pkl2')
