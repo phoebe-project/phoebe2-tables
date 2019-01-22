4dd09b889d92d8d0364c7582fe6d1395e0be239d
Kyle Conroy <kyleconroy@gmail.com>
2019-01-22 13:33:07 -0500

    update_list.py to work with python2 and python3
    * must be run separately
    * add timestamp to dictionaries
    * also remove list_online_passbands file since it hasn't been used since before 2.0.0
    
    wasn't used since before the official 2.0 release

--- passbands/update_list.py
+++ passbands/update_list.py
@@ -1,31 +1,41 @@
 import json
 import os
+import sys
 from glob import glob
 import marshal
+import pickle
+
+"""
+should be run separately under python2 (will create list_online_passbands_full) and python3 (will create list_online_passbands_full_pb3)
+"""
 
 def load_passband(fname):
-    f = open(fname, 'rb')
-    struct = marshal.load(f)
-    f.close()
+    with open(fname, 'rb') as f:
+        if sys.version_info[0] < 3:
+            struct = marshal.load(f)
+        else:
+            struct = pickle.load(f)
 
     info = {}
     info['fname'] = os.path.basename(fname)
     info['atms'] = struct['atmlist']
+    info['timestamp'] = struct.get('timestamp', None)
 
     return struct['pbset']+':'+struct['pbname'], info
 
 pbtable = {}
 
-for fname in glob('./*pb'):
-   print "loading {}...".format(fname)
+for fname in glob('./*pb' if sys.version_info[0] < 3 else './*pb3'):
+   print("loading {}...".format(fname))
    key, info = load_passband(fname)
    pbtable[key] = info
 
-# keep this for legacy sake, for now (development version before Feb 8 will require this)
-f = open('list_online_passbands', 'w')
-f.write(json.dumps({k: v['fname'] for k,v in pbtable.items()}))
-f.close()
+# dump dictionary for Python 2 or 3
+if sys.version_info[0] < 3:
+    fname = 'list_online_passbands_full'
+else:
+    fname = 'list_online_passbands_full_pb3'
 
-f = open('list_online_passbands_full', 'w')
-f.write(json.dumps(pbtable))
-f.close()
+print("writing to {}".format(fname))
+with open(fname, 'w') as f:
+    f.write(json.dumps(pbtable))
